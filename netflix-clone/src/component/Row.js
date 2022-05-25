9d103a1fe341be7b419968b3789ac8bc42dda729
Fazid Samoon <tuan.fazid@outlook.com>
2022-05-26 01:37:50 +0530

    movies added

--- netflix-clone/src/component/Row.js
+++ netflix-clone/src/component/Row.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../styles/row.css"
+import axios from "../axios"
 
-function Row(props) {
+function Row({title , fetchUrl , islargeRow = false}) {
+
+  const [movie , setMovie] = useState([]);
+
+  const baseUrl = "https://image.tmdb.org/t/p/original/";
+  
+  useEffect(()=> {
+    async function fetchData () {
+        const request = await axios.get(fetchUrl);
+        setMovie(request.data.results)
+        return request;
+    }
+    fetchData();
+
+  },[fetchUrl])
+
+  
   return (
     <div className='row'>
-        <h2 className='t'>{props.title}</h2>
+        <h2 className='title'>{title}</h2>
+
+        <div className='row-posters'>
+            {movie.map(movie => (
+
+                ((islargeRow && movie.poster_path) ||
+                (!islargeRow && movie.backdrop_path)) && (
+                    <img className= {`row_poster ${islargeRow && "row-posterLarge"}`} 
+                        src={`${baseUrl}${islargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        alt={movie.name} 
+                    />
+                )
+                
+            ))}
+        </div>
+        
+
     </div>
   )
 }
